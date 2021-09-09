216b1fdfee8f32baa9eabae8b2e199088eb64a90
mpsantos8 <matheus.masan@gmail.com>
2021-09-09 14:49:21 -0300

    Define new Colors on the theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,8 +2,12 @@ import {extendTheme} from '@chakra-ui/react'
 
 export const theme = extendTheme({
   colors:{
+    blue:{
+      '400':'#0D6EF0'
+    },
     gray:{
       '200':'#E5E5E5',
+      '600':'#4B4B4B',
       '700':'#444444'
     }
   },
